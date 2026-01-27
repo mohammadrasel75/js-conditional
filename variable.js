32f89ad2766afb0c6d446b15b324d04d0c41fd62
mohammadrasel75 <devraselmiah75@gmail.com>
2026-01-27 22:47:46 +0600

    updated

--- variable.js
+++ variable.js
@@ -2,4 +2,24 @@
  * VARIABLE ==> container
  */
 
+// number 
 var sunglassPrice = 80;
+
+// string 
+
+var subject = "EEE"
+
+
+// boolean
+var passed = true;
+
+/**
+ * NAMING CONVENTION 
+ * 1.single word 
+ * 2.no quote 
+ * 3.no gap or dash 
+ * 4.can not start with number
+ * 5.can not be a keyword 
+ * 6.prefer cameCase
+ */
+
