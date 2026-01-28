36b3eb54774e25cd877cfd4296630cd23cbe7ff5
mohammadrasel75 <devraselmiah75@gmail.com>
2026-01-28 20:49:05 +0600

    few update for comparison

--- comparison.js
+++ comparison.js
@@ -1,2 +1,19 @@
 // console.log(15 > 10);
-console.log(15 < 10);
+// console.log(5 < 10);
+// console.log(5 == 5 ); 
+// console.log(10 == 5 ); 
+// console.log(10 >= 5 ); 
+// console.log(15 <= 5 ); 
+// console.log( 100 <= 10);
+
+// console.log(10 == 10);
+// console.log(10 == 10.1);
+
+// --------------------------
+
+var peyaraPrice = 40;
+var applePrice = 250;
+
+
+
+
