abea3e05d2840a78dcd0d86d27e8ee3e7cc68778
mohammadrasel75 <devraselmiah75@gmail.com>
2026-01-29 00:00:49 +0600

    Updated console comparision

--- comparison.js
+++ comparison.js
@@ -14,6 +14,16 @@
 var peyaraPrice = 40;
 var applePrice = 250;
 
-console.log(peyaraPrice > applePrice);
+// console.log(peyaraPrice <= applePrice);
+
+/**
+ * ==
+ * ===
+ * 
+ */
+
+console.log(10 == '10');
+
+console.log(120 == 'tomato');
 
 
