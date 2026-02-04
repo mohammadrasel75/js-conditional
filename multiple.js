bfb45ad61b75a5464dfe008a5cf987b24b519f3c
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-04 21:43:02 +0600

    || = OR and && = and er khela

--- multiple.js
+++ multiple.js
@@ -1,11 +1,29 @@
-const salary = 25000;
+const salary = 21001;
 const isBCS = true; 
 const height = 68;
 
-if(salary > 20000 && height > 66){
-    console.log('Su-------patro');
+// if(salary > 20000 && height > 66){
+//     console.log('Su-------patro');
+// }
+
+// else {
+//     console.log('onno patro khuji');
+// }
+
+// if(salary > 25000 || height > 72){
+//     console.log('eso baba kobul');
+// }
+
+// else {
+//     console.log('vaag tui mogbul');
+// }
+
+// more and more condition
+
+if(salary > 25000 || height > 72 || isBCS == true){
+    console.log('eso baba kobul');
 }
 
 else {
-    console.log('onno patro khuji');
+    console.log('vaag tui mogbul');
 }
