4c8fca7c9d742a713d665af1f113a1475cf74fea
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-16 09:59:40 +0600

    update loop test

--- loop.js
+++ loop.js
@@ -2,6 +2,19 @@ const numbers = [12, 98, 45, 63, 21, 72];
 
 console.log(numbers);
 
-for(const num of numbers){
-    console.log(num);
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['orange', 'apple', 'banana'];
+
+// for(){
+
+// }
+
+for (const fruit of fruits) {
+
+    console.log(fruit);
+    console.log('I want to eat')
+
 }
