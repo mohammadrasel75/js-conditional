07257c614b1951b363c5f1b3020a9115548741d1
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-17 10:53:30 +0600

    new update for odd or even numbering

---
+++ decremantal-for-loop.js
@@ -0,0 +1,40 @@
+// let n = 10;
+// while(n >=0){
+//     console.log(n);
+//     n--;
+// }
+
+// odd numbers
+
+// for(let i = 0; i < 20; i++){
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 0; i < 20; i+=2){
+//   console.log(i);
+  
+// }
+
+// give me the list numbers between 1 to thirty divisible by 5
+
+// for(let i = 1; i <=30; i++){
+//     if(i % 5 ===0 ){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i = 1; i <=30; i++){
+//     if(i % 3 ===0 || i %5 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+for(let i = 1; i <=30; i++){
+    if(i % 3 ===0 && i %5 === 0){
+        console.log(i);
+    }
+}
---
+++ decremantal.js
@@ -0,0 +1,10 @@
+for(let i = 0; i <=10; i++){
+    console.log(i);
+}
+
+// decrimantal for loop 
+
+for (let i = 10; i<=10; i--){
+    console.log(i);
+    
+}
