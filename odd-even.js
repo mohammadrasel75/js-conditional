075910342f7fe2f18099ebc1eac16b0f28e17f19
mohammadrasel75 <devraselmiah75@gmail.com>
2026-02-18 11:30:14 +0600

    update how to use

--- odd-even.js
+++ odd-even.js
@@ -6,11 +6,11 @@
 //     console.log(i);
 // }
 
-// even
-for(let i = 0; i <= 20; i=i+2){
-    console.log(i);
+// // even
+// for(let i = 0; i <= 20; i=i+2){
+//     console.log(i);
 
-} 
+// } 
 
 // odd
 for(let i = 1; i <=20; i=i+2){
